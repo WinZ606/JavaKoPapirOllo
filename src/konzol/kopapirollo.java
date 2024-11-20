eaa3bee2cc804ab4d6dd0f462329a0d9c0c4dfb0
Dezso Vince <2005bnm@gmail.com>
2024-11-20 09:40:30 +0100

    alapok keszen

---
+++ src/konzol/kopapirollo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package konzol;
+
+/**
+ *
+ * @author DezsőVinceAndrás(Szf
+ */
+import java.util.Scanner;
+
+public class kopapirollo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Kő/Papír/Olló");
+        
+        String felhasznalo = sc.nextLine();
+        int random = (int) (Math.random());
+
+    }
+}
