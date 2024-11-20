584eb248fb90ebff4a60f52e991443c4f02357ce
Dezso Vince <2005bnm@gmail.com>
2024-11-20 10:11:51 +0100

    majdnem mukodik a dolog

--- src/konzol/kopapirollo.java
+++ src/konzol/kopapirollo.java
@@ -15,9 +15,29 @@ public class kopapirollo {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Kő/Papír/Olló");
-        
+        String gepvalasz2;
         String felhasznalo = sc.nextLine();
         int random = (int) (Math.random());
-
+        
+        String gepvalasz1;
+        if (random == 0) {
+            gepvalasz1 = "Kő";
+        } else if (random == 1) {
+            gepvalasz1 = "Papír";
+        } else {
+            gepvalasz1 = "Olló";
+        }
+        
+        if (gepvalasz1 == "Kő" & felhasznalo == "Kő"){
+            gepvalasz2 = "Döntetlen!";
+        }
+        else if (gepvalasz1 == "Papír" & felhasznalo == "Kő" | gepvalasz1 == "Olló" & felhasznalo == "Papír" | gepvalasz1 == "Kő" & felhasznalo == "Olló"){
+            gepvalasz2 = "A gép nyert!";
+        }
+        else{
+            gepvalasz2 = "A felhasználó nyert";
+        }
+        System.out.println("A gép válasza: " + gepvalasz1);
+        System.out.println("Tehát " + gepvalasz2);
     }
 }
